4773c4eea041aa905b82ceff09ac17ae935330ce
tylkahn <tylkahn@nuevaschool.org>
2016-02-20 16:01:01 -0800

    cmake

---
+++ build/CMakeFiles/highgoal.bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/highgoal.bin.dir/highgoal.cpp.o"
+  "highgoal.bin.pdb"
+  "highgoal.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/highgoal.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
