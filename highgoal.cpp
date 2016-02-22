eb6dbdbaa6c3852779309bf759f2670950a5cec5
tylkahn <tylkahn@nuevaschool.org>
2016-02-21 19:39:06 -0800

    merge master

--- highgoal.cpp
+++ highgoal.cpp
@@ -1,5 +1,4 @@
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/features2d.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
@@ -9,6 +8,8 @@
 
 using namespace cv;
 using namespace std;
+
+Mat src, src_gray, subtracted;
 
 int thresh = 210;
 int max_thresh = 255;
@@ -69,6 +70,9 @@ int getdir (string dir, vector<string> &files) {
     closedir(dp);
     return 0;
 }
+
+
+
 
 int main(int argc, char** argv) {
     string image = "latest.jpg";
@@ -175,42 +179,44 @@ void analyzeImage(Mat src) {
 void convex_callback(int, void* ) {
     Mat threshold_output, convex;
     // vector<vector<Point> > contours;
-
-int tyler()
-{
-    Mat src;
-    src = imread("pict.jpg", CV_LOAD_IMAGE_COLOR);
-    Mat gray;
-    cvtColor(src, gray, CV_BGR2GRAY);
-    threshold(gray, gray,200, 255,THRESH_BINARY_INV); //Threshold the gray
-    imshow("gray",gray);int largest_area=0;
-    int largest_contour_index=0;
-    Rect bounding_rect;
-    vector<vector<Point> > contours; // Vector for storing contour
     vector<Vec4i> hierarchy;
-    findContours( gray, contours, hierarchy,CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
-    // iterate through each contour.
-    for( int i = 0; i< contours.size(); i++ )
-    {
-        //  Find the area of contour
-        double a=contourArea( contours[i],false);
-        if(a>largest_area){
-            largest_area=a;
-            cout<<i<<" area  "<<a<<endl;
-            // Store the index of largest contour
-            largest_contour_index=i;
-            // Find the bounding rectangle for biggest contour
-            bounding_rect=boundingRect(contours[i]);
-        }
+
+    threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
+    if (gui && detailedGUI) imshow("threshold",threshold_output);
+    findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+    vector<vector<Point> >hull( contours.size() );
+    for( int i = 0; i < contours.size(); ++i ) {  convexHull( Mat(contours[i]), hull[i], false ); }
+
+    convex = Mat::zeros( threshold_output.size(), CV_8UC1 );
+    for (int i = 0; i<contours.size(); ++i) {
+        drawContours(convex, hull, i, Scalar(255,255,255), CV_FILLED, 8, vector<Vec4i>(), 0, Point() );
     }
-   Scalar color( 255,255,255);  // color of the contour in the
-   //Draw the contour and rectangle
-   drawContours( src, contours,largest_contour_index, color, CV_FILLED,8,hierarchy);
-   rectangle(src, bounding_rect,  Scalar(0,255,0),2, 8,0);
-   namedWindow( "Display window", CV_WINDOW_AUTOSIZE );
-   imshow( "Display window", src );
-   waitKey(0);
-   return 0;
+
+    subtracted = Mat::zeros(convex.size(), CV_8UC1);
+
+    if(convex.isContinuous()&&threshold_output.isContinuous()) {    uchar *p1, *p2, *p3;
+        p1 = convex.ptr<uchar>(0);
+        p2 = threshold_output.ptr<uchar>(0);
+        p3 = subtracted.ptr<uchar>(0);
+        for (int i=0; i<convex.rows*convex.cols; ++i){
+            if (*p2 != 0){
+                *p3 = 0;
+            }
+            else if(*p1 != 0){
+                *p3 = 255;
+            }
+            p1++;
+            p2++;
+            p3++;
+        }
+
+    }
+    // subtract(convex, threshold_output, subtracted);
+
+    if (gui && detailedGUI) imshow("convex", convex);
+    if (gui && detailedGUI) imshow("subtracted", subtracted);
+    blob_callback(0,0);
 }
 
 void blob_callback(int, void*) {
