eeac87a30dd641c671937ba9f570ca6941719d83
oshlern <oshlern@nuevaschool.org>
2016-02-22 17:32:02 -0800

    Added commented out code which s my suggestion for running only when we get new images

--- update.py
+++ update.py
@@ -1,7 +1,10 @@
 import SocketServer
 import subprocess
-while (True): #make a temp var of the latest image, then say while tempvar!==latest, do that stuff
+# currentImage=subprocess.Popen("latest")
+while (True): #while (currentImage!==subprocess.Popen("latest"))
+    # currentImage=subprocess.Popen("latest")
     process = subprocess.Popen(["./highgoal.bin", "latest"], stdout=subprocess.PIPE)
     response = process.stdout.read()
     test = open("test.txt","w+")
     test.write(response)
+# I'm not sure how to actually load the image and save it to currentImage, and if we can compare to images with the !==, but I might be able to write a really quick algorithm to compare them (another opencv file), and shaheen can figure out the loading image part
