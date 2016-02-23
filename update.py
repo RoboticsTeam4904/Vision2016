58ac3e0ca80c32c18fb15957b782563c5cca934f
oshlern <oshlern@nuevaschool.org>
2016-02-22 16:01:17 -0800

    added comment

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 import SocketServer
 import subprocess
-while (True):
+while (True): #make a temp var of the latest image, then say while tempvar!==latest, do that stuff
     process = subprocess.Popen(["./highgoal.bin", "latest"], stdout=subprocess.PIPE)
     response = process.stdout.read()
     test = open("test.txt","w+")
